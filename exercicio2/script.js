80c678cceb7dc809fede73a70e9248b8b86c6e9a
Arthuramaral100 <arthuramaral57@gmail.com>
2022-08-31 15:45:06 -0300

    Exercicios da aula a0009-strings

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,10 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+//a)
+const semEspaco = minhaString.trim()
+console.log(semEspaco);
+
+//b)
+console.log("Caracteres da string antes da remoção de espaços: " + minhaString.length + "\nCaracteres da string depois da remoção de espaços: " + semEspaco.length);
+
+//c)
+console.log(semEspaco.replace("________", "sticioso"));
